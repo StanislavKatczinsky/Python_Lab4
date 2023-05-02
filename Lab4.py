7dfa1fa39dc1b8090aeb196f5de5c53f1cb198fe
Митюшин Станислав Михайлович <SMikMityushin@sberbank.ru>
2023-05-02 12:44:01 +0300

    initial commit

---
+++ Lab4.py
@@ -0,0 +1,47 @@
+class Poly:
+    def __init__(self, coef_dict):
+        self.coef_dict = coef_dict
+    
+    def __repr__(self):
+        # Здесь будет реализация метода
+        pass
+    
+    def __neg__(self):
+        # Здесь будет реализация метода
+        pass
+    
+    def __add__(self, other):
+        # Здесь будет реализация метода
+        pass
+    
+    def __sub__(self, other):
+        # Здесь будет реализация метода
+        pass
+    
+    def __mul__(self, other):
+        # Здесь будет реализация метода
+        pass
+    
+    def __rmul__(self, other):
+        # Здесь будет реализация метода
+        pass
+    
+    def __truediv__(self, other):
+        # Здесь будет реализация метода
+        pass
+    
+    def eval(self, x):
+        # Здесь будет реализация метода
+        pass
+    
+    def sub(self, other):
+        # Здесь будет реализация метода
+        pass
+    
+    def diff(self, n=1):
+        # Здесь будет реализация метода
+        pass
+    
+    def integrate(self, n=1, a=0, b=1):
+        # Здесь будет реализация метода
+        pass
